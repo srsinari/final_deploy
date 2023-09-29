be09944a2edf6a2ab1c50407e10708eae9c3a75e
Srsinari <sunnysinari0018@gmail.com>
2023-09-29 16:42:24 +0530

    first commit

--- src/pages/birthday.jsx
+++ src/pages/birthday.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { calculateTimeDifference } from './cal.js';
 
 function BirthdayPage() {
-  const herBirthdate = '1994-10-04'; // Replace with her actual birthdate
+  const herBirthdate = '1994-11-04'; // Replace with her actual birthdate
   const herBirthTime = '11:05:00'; // Replace with her actual time of birth
 
   const [timeDifference, setTimeDifference] = useState(0);
@@ -47,8 +47,7 @@ function BirthdayPage() {
     <div>
       
       <p style={{fontFamily:"'Playfair Display', serif", color:"rgba(244, 6, 137, 0.905)", fontSize:"25px"}}>
-      Celebrating {years} years, {months} months,  {days} days, 
-      {hours} hours,  {minutes} minutes, <br/> and  {seconds} seconds of an 
+      Celebrating {years} years, {months} months,  {days} days, {hours} hours,  {minutes} minutes, <br/> and  {seconds} seconds of an 
       amazing journey since you graced this<br/> world with your presence!
       </p>
       {/* Add your birthday wishes here */}
